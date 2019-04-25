0c9d8895de7b40335135c3296cdeb262c768445d
vbhv007 <bhardwajvbhv@gmail.com>
2019-04-25 12:16:35 +0530

    added post route and updated get route

--- main.py
+++ main.py
@@ -9,9 +9,36 @@ app.config['MONGO_URI'] = 'mongodb+srv://vbhv:qwerty123@clipboarddb-vm1cu.mongod
 mongo = PyMongo(app)
 
 
-@app.route('/', methods=['GET'])
-def get_the_page():
-    return jsonify({'result': 'this is the result'})
+@app.route('/<urlString>', methods=['GET'])
+def get_the_page(urlString):
+    publicData = mongo.db.publicData
+    query = publicData.find_one({'urlString': urlString})
+    if query:
+        output = {
+            'Success': True, 'urlString': query['urlString'], 'bodyData': query['bodyData']}
+    else:
+        output = {'Success': False, 'Comment': 'No data found'}
+    return jsonify({'results': output})
+
+
+@app.route('/<urlString>', methods=['POST'])
+def save_the_page(urlString):
+    publicData = mongo.db.publicData
+    bodyData = request.json['bodyData']
+
+    # finding if the urlString is already taken
+    query = publicData.find_one({'urlString': urlString})
+    if not query:
+        publicDataId = publicData.insert(
+            {'urlString': urlString, 'bodyData': bodyData})
+
+        data = publicData.find_one({'_id': publicDataId})
+        output = {
+            'Success': True, 'urlString': data['urlString'], 'bodyData': data['bodyData']}
+    else:
+        output = {'Success': False, 'Comment': 'Url already exists'}
+
+    return jsonify({'results': output})
 
 
 if __name__ == '__main__':
