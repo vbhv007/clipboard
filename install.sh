2185f47db17eef0dc8a1d7d60620454ef2a97bca
vbhv007 <bhardwajvbhv@gmail.com>
2019-05-15 19:38:13 +0530

    file already exists fixed

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "Setting up Clipboard. Stay back and relax..."
-chmod +x clipboard.py
-mkdir ~/bin
+chmod +x clipboard
+FILE=~/bin
+if [ -d "$FILE" ]; then
+    echo "~/bin already exists. Cool!"
+else
+    mkdir ~/bin
+fi
 cp clipboard ~/bin
 echo 'export PATH=$PATH":$HOME/bin"' >> ~/.profile
-source .profile
+source ~/.profile
 echo "Done!.. Take it away."
