81c0a1b509010b10b507a13e0419cccc21a17e9f
vbhv007 <bhardwajvbhv@gmail.com>
2019-05-16 19:55:40 +0530

    now it won't pollute .profile

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 echo "Setting up Clipboard. Stay back and relax..."
+echo
 chmod +x clipboard
-FILE=~/bin
-if [ -d "$FILE" ]; then
+FILE1=~/bin
+FILE2=~/.profile
+if [ -d "$FILE1" ]; then
     echo "~/bin already exists. Cool!"
+    echo
+    rm ~/bin/clipboard
 else
     mkdir ~/bin
 fi
 cp clipboard ~/bin
-echo 'export PATH=$PATH":$HOME/bin"' >> ~/.profile
-source ~/.profile
+if grep -q 'export PATH=$PATH":$HOME/bin"' "$FILE2"; then
+    echo "Seems like this is not the first time you are installing this."
+    echo
+else
+    echo 'export PATH=$PATH":$HOME/bin"' >> ~/.profile
+    source ~/.profile
+fi
 echo "Done!.. Take it away."
