801c2cd3dafcfacacfa89374e928c3c9cf4a6901
vbhv007 <bhardwajvbhv@gmail.com>
2020-04-26 22:12:30 +0530

    updated python3 env path

--- install.sh
+++ install.sh
@@ -3,21 +3,47 @@
 echo "Setting up Clipboard. Stay back and relax..."
 echo
 chmod +x clipboard
-FILE1=~/bin
-FILE2=~/.profile
+FILE1=~/.clipboard
+BASHRC=~/.bashrc
+ZSHRC=~/.zshrc
+BASHPROFILE=~/.bash_profile
 if [ -d "$FILE1" ]; then
-    echo "~/bin already exists. Cool!"
+    echo "~/.clipboard already exists. Cool!"
     echo
-    rm ~/bin/clipboard
+    rm ~/.clipboard/clipboard
 else
-    mkdir ~/bin
+    mkdir ~/.clipboard
 fi
-cp clipboard ~/bin
-if grep -q 'export PATH=$PATH":$HOME/bin"' "$FILE2"; then
-    echo "Seems like this is not the first time you are installing this."
-    echo
-else
-    echo 'export PATH=$PATH":$HOME/bin"' >> ~/.profile
-    source ~/.profile
+cp clipboard ~/.clipboard
+
+if test -f "$BASHRC"; then
+    if grep -q 'export PATH=$PATH":$HOME/.clipboard"' "$BASHRC"; then
+        echo "Seems like this is not the first time you are installing this."
+        echo
+    else
+        echo 'export PATH=$PATH":$HOME/.clipboard"' >> $BASHRC
+        source $BASHRC
+    fi
 fi
+
+if test -f "$ZSHRC"; then
+    if grep -q 'export PATH=$PATH":$HOME/.clipboard"' "$ZSHRC"; then
+        echo "Seems like this is not the first time you are installing this."
+        echo
+    else
+        echo 'export PATH=$PATH":$HOME/.clipboard"' >> $ZSHRC
+        source $ZSHRC
+    fi
+fi
+
+if test -f "$BASHPROFILE"; then
+    if grep -q 'export PATH=$PATH":$HOME/.clipboard"' "$BASHPROFILE"; then
+        echo "Seems like this is not the first time you are installing this."
+        echo
+    else
+        echo 'export PATH=$PATH":$HOME/.clipboard"' >> $BASHPROFILE
+        source $BASHPROFILE
+    fi
+fi
+
 echo "Done!.. Take it away."
